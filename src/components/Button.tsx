882dc55c6a32e2a6dca588cec47a4cdace725c66
Mustafa1121 <mustafa.khodor11@gmail.com>
2024-10-06 10:06:34 +0300

    push lib

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ title }) => {
+  return <div>{title}</div>;
+};
+
+export default Button;
